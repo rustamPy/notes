94ee7f591b706dcd2a70de36209ae3af82538f82
Rustam <kerimov.rustam@live.ru>
2023-02-08 01:59:52 +0100

    pox

--- find_wordle.py
+++ find_wordle.py
@@ -1,16 +1,22 @@
+from typing import Any
+
 import urllib3
 import random
+
 
 class Game:
     def __init__(self, wordle: bool = True):
         if wordle:
-            self.URL = 'https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/ca9018b32e963292473841fb55fd5a62176769b5/valid-wordle-words.txt'
+            self.URL = 'https://gist.githubusercontent.com/dracos/' \
+                       'dd0668f281e685bad51479e5acaadb93/raw/ca901' \
+                       '8b32e963292473841fb55fd5a62176769b5/valid-' \
+                       'wordle-words.txt'
         else:
             self.URL = 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt'
-        
+
     def find_word(self, include: str = None, exclude: str = None, begins_with: str = None, ends_with: str = None,
-                  length: int = None, order: str = None, disorder: str = None, min_size: int = None, max_size: int = None,
-                  include_only: str = None, wordle: bool = True) -> list:
+                  length: int = None, order: str = None, disorder: Any = None, min_size: int = None,
+                  max_size: int = None, include_only: str = None) -> list:
         """
         :param include: the words will have al of these letters
         :param exclude: the words will not have all of these letters
@@ -22,7 +28,6 @@ class Game:
         :param min_size: the words will have minimum this number of letters
         :param max_size: the words will have maximum this number of letters
         :param include_only: the words will only include this set of values
-        :param wordle: turn on wordle words
         :return: list
         """
 
@@ -41,15 +46,16 @@ class Game:
                            all([char.lower() in include_only.lower() for char in w])] if include_only else exclude_base
 
         begins_with_ls = [w for w in include_only_ls if w.startswith(begins_with)] if begins_with else include_only_ls
-        ends_with_ls = [w for w in begins_with_ls if w.lower().endswith(ends_with.lower())] if ends_with else begins_with_ls
+        ends_with_ls = [w for w in begins_with_ls if
+                        w.lower().endswith(ends_with.lower())] if ends_with else begins_with_ls
 
-        order_ls = [w for w in ends_with_ls if all([w[i] == order[i] for i in range(len(order)) if order[i]!='_'])] if order else ends_with_ls
-        disorder_ls = [word for word in order_ls 
-                       if (not isinstance(disorder, list) and all([word[i] != disorder[i] for i in range(len(disorder))]))  or
-                       (isinstance(disorder, list) and all([all([word[i] != disord[i] for i in range(len(disord))]) 
+        order_ls = [w for w in ends_with_ls if
+                    all([w[i] == order[i] for i in range(len(order)) if order[i] != '_'])] if order else ends_with_ls
+        disorder_ls = [word for word in order_ls
+                       if (not isinstance(disorder, list) and all(
+                [word[i] != disorder[i] for i in range(len(disorder))])) or
+                       (isinstance(disorder, list) and all([all([word[i] != disord[i] for i in range(len(disord))])
                                                             for disord in disorder]))] if disorder else order_ls
-
-
 
         return disorder_ls
 
@@ -68,13 +74,10 @@ class Game:
                 return rand
         return random.choice(ls)
 
+
 if __name__ == "__main__":
     obj = Game()
     example = obj.find_word(exclude='rdiojcksnum', disorder=['f____'], order='_a___', include='af')
     print(obj.lucky_you(example))
     print(len(example))
     print(example)
-    
-    
-    
-    
