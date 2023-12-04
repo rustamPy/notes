aa8ccd0c98f62ca4fac383f2de29ba39ad655339
Rustam Karimov <35258613+rustamPy@users.noreply.github.com>
2023-12-04 07:03:00 +0100

    Fix

--- sozdenReqeme.py
+++ sozdenReqeme.py
@@ -1,36 +1,39 @@
 class Solution:
-    def sozdenReqeme(self,s):
+    def sozdenReqeme(self, s):
         sozler = {'sıfır': 0,
-                'bir': 1,
-                'iki': 2,
-                'üç': 3,
-                'dörd': 4,
-                'beş': 5,
-                'altı': 6,
-                'yeddi': 7,
-                'səkkiz': 8,
-                'doqquz': 9,
-                'on': 10,
-                'iyirmi': 20,
-                'otuz': 30,
-                'qırx': 40,
-                'əlli': 50,
-                'altmış': 60,
-                'yetmiş': 70,
-                'səksən': 80,
-                'doxsan': 90,
+                  'bir': 1,
+                  'iki': 2,
+                  'üç': 3,
+                  'dörd': 4,
+                  'beş': 5,
+                  'altı': 6,
+                  'yeddi': 7,
+                  'səkkiz': 8,
+                  'doqquz': 9,
+                  'on': 10,
+                  'iyirmi': 20,
+                  'otuz': 30,
+                  'qırx': 40,
+                  'əlli': 50,
+                  'altmış': 60,
+                  'yetmiş': 70,
+                  'səksən': 80,
+                  'doxsan': 90,
                   }
-    
-        ls=[]
+
+        ls = []
         for i in s.split(' '):
             if i in sozler:
                 ls.append(sozler[i])
-            elif i=="yüz":
-                ls[-1]*=100
-            elif i=="min":
-                ls=[i*1000 for i in ls]
-            elif i=="milyon":
-                ls=[i*1000000 for i in ls]
+            elif i == "yüz":
+                ls[-1] *= 100 if ls else [100]
+            elif i == "min":
+                ls = [i * 1000 for i in ls] if ls else [1000]
+            elif i == "milyon":
+                ls = [i * 1000000 for i in ls] if ls else [1000000]
+
         return sum(ls)
-p=Solution()
-print(p.sozdenReqeme("altı min iyirmi üç"))
+
+
+p = Solution()
+print(p.sozdenReqeme("min beş yüz altı"))
